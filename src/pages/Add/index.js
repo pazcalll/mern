4710bbec972a6b3d420e25b27340a17d2659d457
pazcalll <yazeedarifin@gmail.com>
2021-08-18 22:40:57 +0700

    form insert data

--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css' 
 import { Input, Button } from '../../components'
+import axios from 'axios'
 
 const Add = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+    const onSubmit = () => {
+        console.log('title: ', title)
+        console.log('body: ', body)
+
+        // const data = new FormData();
+        // data.append('name', title)
+        // data.append('goodsToChannel', body)
+
+        const data = {
+            name: title,
+            goodsToChannel: body
+        }
+
+        console.log(data)
+        axios.post('http://localhost:4000/goods', data)
+        .then(res=>{
+            console.log("insert data: ", res)
+        })
+        .catch(err =>{
+            console.log("error: ", err)
+        })
+    }
+
     return (
         <div className="add">
             <div className="card">
@@ -11,10 +38,10 @@ const Add = () => {
                 </div>
                 <div className="card-body">
                     <form>
-                        <Input label="Item Name" placeholder="Item Name"/>
-                        <Input label="Amount" placeholder="Number" type="number"/>
+                        <Input label="Item Name" placeholder="Item Name" onChange={(e)=>setTitle(e.target.value)}/>
+                        <Input label="Channel" placeholder="Channel" onChange={(e)=>setBody(e.target.value)}/>
                         <br/>
-                        <Button title="Enter" type="submit"/>
+                        <Button title="Enter" onClick={onSubmit} type="submit"/>
                     </form>
                 </div>
             </div>
